c978b6f5071fc23223f2efcde147c95fe1914464
郑涛 <zhengT0310@aliyun.com>
2025-07-31 17:08:04 +0800

    fix: 国际化处理

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -20,7 +20,7 @@ const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
  */
 const manifest = {
   manifest_version: 3,
-  default_locale: 'en',
+  default_locale: 'zh_CN',
   name: '__MSG_extensionName__',
   browser_specific_settings: {
     gecko: {
